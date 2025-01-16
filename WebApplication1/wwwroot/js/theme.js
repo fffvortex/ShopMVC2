344e8f5aecfa97177db0ce6b8a25c76a6f0fc7f3
fffvortex <fvortex@mail.ru>
2025-01-16 14:16:59 +0300

    fixed header

--- WebApplication1/wwwroot/js/theme.js
+++ WebApplication1/wwwroot/js/theme.js
@@ -1,24 +1,41 @@
-function setTheme() {
-    document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'))
-}
 let check = document.querySelector('.fa-circle-half-stroke')
 let checkbox = document.getElementById("checkbox")
+let header = document.getElementsByClassName('sticky-top')
+
+function setTheme() {
+    document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'))
+    if (localStorage.getItem('theme') === 'dark') {
+        for (let el of header) {
+            el.classList.remove('top-light')
+            el.classList.add('top-dark')
+        }
+    }
+    else {
+        for (let el of header) {
+            el.classList.remove('top-dark')
+            el.classList.add('top-light')
+        }
+    }
+}
+
 checkbox.addEventListener("click", () => {
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
         document.documentElement.setAttribute('data-bs-theme', 'light')
+        localStorage.setItem('theme', 'light')
 
         check.classList.add('fa-spin')
         setTimeout(() => {
             check.classList.remove('fa-spin')
         }, 2000)
-        localStorage.setItem('theme', 'light')
+        setTheme()
     }
     else {
         document.documentElement.setAttribute('data-bs-theme', 'dark')
+        localStorage.setItem('theme', 'dark')
         check.classList.add('fa-spin')
         setTimeout(() => {
             check.classList.remove('fa-spin')
         }, 2000)
-        localStorage.setItem('theme', 'dark')
+        setTheme()
     }
 })
